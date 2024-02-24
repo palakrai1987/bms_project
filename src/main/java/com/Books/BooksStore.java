01db6e2016cae9c442f71d9e7f79522fb5aeda56
palakrai1987 <its.palak11@gmail.com>
2024-02-24 12:43:50 +0530

    "NEW"

---
+++ src/main/java/com/Books/BooksStore.java
@@ -0,0 +1,55 @@
+package com.Books;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import com.db.DatabseConnection;
+@WebServlet("/book_register")
+public class BooksStore extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+    public BooksStore()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException 
+	
+	{
+		try {
+			  String bname = req.getParameter("name");
+			  String bedition = req.getParameter("bedition");
+			 // boolean f = false;
+			  Connection con = DatabseConnection.getConn();
+			  String sql = "insert into book(bname,bedition) values (?,?)";
+			  
+				PreparedStatement ps = con.prepareStatement(sql);
+				 ps.setString(1,bname);
+			      ps.setString(2,bedition);
+				
+			     
+			    int  i = ps.executeUpdate();
+			  if(i>0)
+			    {System.out.println("data inserted successfully");}
+			    else {System.out.println("error");}
+			  
+			  
+			 }catch(Exception e)
+			 {
+				 e.printStackTrace();
+			 }
+				
+	}
+
+}
---
+++ src/main/webapp/Register.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+    <h2>user registration</h2>
+<form action = "book_register" method="post">
+    <input type= "text" name = "name">BookName <br>
+    <input type= "text" name = "bedition">BookEdition <br>
+    
+    <input type = "submit" value = "submit">
+    </form>
+
+</body>
+</html>
